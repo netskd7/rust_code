d996cf37659d9a9c6722f86ee149b611116c39e4
princ1ple <382527885@qq.com>
2024-02-02 22:37:56 +0800

    first commit

---
+++ rsrsrs/src/main.rs
@@ -0,0 +1,26 @@
+use std::{cmp::Ordering, io};
+
+use rand::Rng;
+
+fn main() {
+    println!("Guess the number!");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    // println!("The secret number is {secret_number}");
+    loop {
+        println!("Please input ur guess: ");
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Fail to read a line...");
+        // println!("U guessed {guess}");
+        let guess: u32 = guess.trim().parse().expect("Please type a number.");
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You're correct!");
+                break;
+            }
+        }
+    }
+}
